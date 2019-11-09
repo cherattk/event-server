b80062d339793e6fe3275592d2becec65c6e11b6
cheratt karim <karim.cheratt@yahoo.ca>
2019-11-09 00:24:43 -0500

    fix demo

--- demo/listener.js
+++ demo/listener.js
@@ -1,44 +1,86 @@
 const http = require('http');
 
-var DataStore = [];
+var ListenerDataStore = [];
 
 const listener = [
-  {hostname : 'localhost' , port : '3001'},
-  {hostname : 'localhost' , port : '3002'},
-  {hostname : 'localhost' , port : '3003'},
-  {hostname : 'localhost' , port : '3004'},
-  {hostname : 'localhost' , port : '3005'}
+  {
+    hostname: 'localhost', port: '3001',
+    description: 'Convert message to binary notation',
+    format: 2
+  },
+  {
+    hostname: 'localhost', port: '3002',
+    description: 'Convert message to octal notation',
+    format: 8
+  },
+  {
+    hostname: 'localhost', port: '3003',
+    description: 'Convert message to hexadecimal notation',
+    format: 16
+  }
 ];
 
-listener.forEach(function(obj, idx){
+
+/**
+ * 
+ */
+
+const dataServer = { port: 3000, hostname: 'localhost' };
+http.createServer((Request, Response) => {
+
+  Response.statusCode = 200;
+  Response.setHeader("Access-Control-Allow-Origin", "*");
+  Response.setHeader('Content-Type', 'text/plain');
+  Response.end(JSON.stringify(ListenerDataStore));
+
+}).listen(dataServer.port, dataServer.hostname, () => {
+  console.log(`data server running at http://${dataServer.hostname}:${dataServer.port}`);
+})
+
+/**
+ * 
+ */
+listener.forEach(function (obj, idx) {
 
   var serverIndex = idx + 1;
+  var listenerEndpoint = `http://${obj.hostname}:${obj.port}/`;
+
   http.createServer((Request, Response) => {
 
+    Response.statusCode = 404;
     Response.setHeader("Access-Control-Allow-Origin", "*");
     Response.setHeader('Content-Type', 'text/plain');
 
-    if(Request.method === 'POST'){
+    var path = Request.url;
+    if (path === '/push_event' && Request.method === 'POST') {
       var body = '';
-      Request.on('data', function(data){
-        body += data;
-      }).on('end', () => {  
-        console.log(`================= listener ${serverIndex} ==================\n`); 
-        console.log('received message'); 
-        console.log(body); 
-        console.log(`======================================================\n`); 
+      Request.on('data', function (data) {
+        body += data.toString();
+      }).on('end', () => {
+        console.log(`================= listener ${serverIndex} ==================\n`);
+        console.log('received message');
+        console.log(body);
+        console.log(`======================================================\n`);
+
         Response.statusCode = 200;
+        ListenerDataStore[idx] = {
+          listener: listenerEndpoint,
+          description : listener[idx].description,
+          event_data: body,
+          processed_data: (function (str) {
+            var result = '';
+            for (var i = 0; i < str.length; i++) {
+              result += Number(str.charCodeAt(i)).toString(obj.format);
+            }
+            return result;
+          })(body)
+        }
       });
     }
-    else{
-      Response.statusCode = 404;
-    }
-
-    Response.end(JSON.stringify(body));
-    
+    Response.end();
   })
-  .listen(obj.port, obj.hostname, () => {    
-    console.log(`listener (${serverIndex}) running at http://${obj.hostname}:${obj.port}/`);
-  });
+    .listen(obj.port, obj.hostname, () => {
+      console.log(`listener (${serverIndex}) running at ${listenerEndpoint}`);
+    });
 
 });
--- demo/publisher.js
+++ demo/publisher.js
@@ -1,31 +1,55 @@
-$(function() {
+$(function () {
 
   const dispatcher_url = 'http://localhost:8080/dispatch';
 
   $('#dispatch-endpoint').append(dispatcher_url);
 
+  ///////////////////////////////////////////////////////////
+
+  var loadListenerData = function () {
+    const dataServer = 'http://localhost:3000';
+
+    $.getJSON(dataServer, function (data) {
+      var list = '';
+      if(data.length){
+        data.forEach(element => {
+          list += '<li>';
+          list += 'Listener : ' + element.listener + '<br/>';
+          list += 'Description : ' + element.description + '<br/>';
+          list += 'Original : ' + element.event_data + '<br/>';
+          list += 'Result : ' + element.processed_data;
+          list += '</li>';
+        });
+      }
+      else{
+        list += '<li>No Listeners data</li>';
+      }
+
+      $('#listener-result').html(list);
+    });
+  }
 
   ///////////////////////////////////////////////////////
-  $( "#pub-form" ).submit(function(e){
+  $("#pub-form").submit(function (e) {
     e.preventDefault();
     var message = {
-      event_id : e.target.elements['event_id'].value,
-      message : e.target.elements['published_message'].value
+      event_id: e.target.elements['event_id'].value,
+      message: e.target.elements['published_message'].value
     };
     $.ajax({
       type: "POST",
       url: dispatcher_url,
-      dataType: 'json',
       data: message,
-      success: function(data , textStatus){
-        console.log(data);
+      success: function (data, textStatus) {
+        loadListenerData();
       },
+      error : function(error){
+        console.log(error);
+      }
     });
   });
-  
-  ///////////////////////////////////////////////////////
-  $('#get-listener-result').click(function(){
-    alert('test');
-  });
+
+  // init 
+  loadListenerData();
 
 });
